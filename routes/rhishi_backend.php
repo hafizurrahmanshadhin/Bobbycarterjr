b2bc47e6a4ea1f7d36f5d337b339c9f467ca12d5
Rhishi Kesh <reshikash300@gmail.com>
2024-10-10 15:36:01 +0600

    Subscription & Subscription Details Complete

--- routes/rhishi_backend.php
+++ routes/rhishi_backend.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\Web\Backend\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/subscription', [SubscriptionController::class, 'index'])->name('admin.subscription.index');
-Route::get('/subscription/edit/{id}', [SubscriptionController::class, 'edit'])->name('admin.subscription.edit');
-Route::post('/subscription/update/{id}', [SubscriptionController::class, 'update'])->name('admin.subscription.update');
+Route::controller(SubscriptionController::class)->name('admin.')->group(function () {
+    Route::get('/subscription', 'index')->name('subscription.index');
+    Route::get('/subscription/edit/{id}', 'edit')->name('subscription.edit');
+    Route::post('/subscription/update/{id}', 'update')->name('subscription.update');
+});
