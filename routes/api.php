0853b74ce51d51c88adca5a6761a28d5398c5f44
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2024-10-10 11:04:24 +0600

    added social login and reset url

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\PasswordResetController;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\SocialLoginController;
 use Illuminate\Support\Facades\Route;
 
 //! Auth Routes
@@ -13,3 +14,6 @@ Route::post('/logout', [LogoutController::class, 'Logout'])->middleware('auth:sa
 Route::post('/send-otp', [PasswordResetController::class, 'SendOtpToEmail']);
 Route::post('/verify-otp', [PasswordResetController::class, 'VerifyOTP']);
 Route::post('/reset-password', [PasswordResetController::class, 'ResetPassword']);
+
+//! Route For Socialite Login.
+Route::post('/social-login', [SocialLoginController::class, 'SocialLogin']);
