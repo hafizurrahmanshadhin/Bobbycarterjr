1e6509863f89da2aa5eb1ac7168dc1bc3e788ce2
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2024-10-22 10:40:47 +0600

    scheduler complete

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Console\Commands\CheckExpiredSubscriptions;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Schedule::command(CheckExpiredSubscriptions::class)->hourly();
+// Schedule::command(CheckExpiredSubscriptions::class)->everyMinute();
+Schedule::command(CheckExpiredSubscriptions::class)->daily();
