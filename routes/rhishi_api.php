2f07c826ddd43ba2574411dacca8db5fcd883cbf
Rhishi Kesh <reshikash300@gmail.com>
2024-10-24 10:09:20 +0600

    API under auth

--- routes/rhishi_api.php
+++ routes/rhishi_api.php
@@ -14,16 +14,12 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\VoiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(SubscriptionController::class)->group(function () {
-    Route::get('/package/free', 'freePackage');
-    Route::get('/package/premium', 'premiumPackage');
-});
-
-Route::controller(VoiceController::class)->group(function () {
-    Route::get('rrr/{text}', 'rrr');
-});
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::controller(SubscriptionController::class)->group(function () {
+        Route::get('/package/free', 'freePackage');
+        Route::get('/package/premium', 'premiumPackage');
+    });
 
     Route::controller(UserController::class)->group(function () {
         Route::get('/user/data', 'userData');
