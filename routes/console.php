7158e89cb768fddc2ff17c6d7ea5f7675fbeb02e
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2024-11-02 14:35:57 +0600

    Refactor scheduling for daily affirmations to support dynamic user-defined notification times

--- routes/console.php
+++ routes/console.php
@@ -1,17 +1,25 @@
 <?php
 
 use App\Console\Commands\CheckExpiredSubscriptions;
-use App\Console\Commands\SendDailyAffirmations;
 use App\Console\Commands\SendReminders;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schedule;
 
+//! Static scheduling
 Schedule::command(CheckExpiredSubscriptions::class)->daily();
-
-Schedule::command(SendDailyAffirmations::class)->dailyAt('08:00');
-Schedule::command(SendDailyAffirmations::class)->dailyAt('12:00');
-Schedule::command(SendDailyAffirmations::class)->dailyAt('18:00');
-
-// Schedule::command(SendDailyAffirmations::class)->everyMinute();
-
-// Schedule::command('send:reminders')->everyMinute();
 Schedule::command(SendReminders::class)->everyMinute();
+
+//* Dynamic scheduling based on user-specified notification times
+// Schedule::call(function () {
+//     //! Retrieve unique notification times from `user_affirmations`
+//     $uniqueNotificationTimes = UserAffirmation::whereNotNull('notification_time')
+//         ->distinct()
+//         ->pluck('notification_time');
+
+//     foreach ($uniqueNotificationTimes as $time) {
+//         Schedule::command(SendDailyAffirmations::class, [$time])->dailyAt($time);
+//     }
+// });
+
+// Run the SendDailyAffirmations command every minute, checking for matching notification times
+Schedule::command('send:daily-affirmations', [Carbon::now()->format('H:i')])->everyMinute();
