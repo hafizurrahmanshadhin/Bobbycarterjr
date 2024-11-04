ba2ab78cc9d0248618d0e5d0989760c344faaac0
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2024-11-04 11:51:02 +0600

    Add channels route and broadcasting configuration to app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
         then: function () {
             Route::middleware(['web', 'auth', 'admin'])
@@ -31,6 +32,15 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->prefix('api')
                 ->group(base_path('routes/rhishi_api.php'));
         },
+    )
+    ->withBroadcasting(
+        __DIR__ . '/../routes/channels.php',
+        [
+            'prefix'     => 'api',
+            'middleware' => [
+                'api', 'auth.sanctum',
+            ],
+        ],
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
