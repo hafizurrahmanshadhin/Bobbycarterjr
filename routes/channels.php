b6b2079e03e14fdf68c4100ff1453c3d2356ac59
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2024-11-04 12:30:46 +0600

    Rename user status broadcast channel and retain chat channel for user authentication

--- routes/channels.php
+++ routes/channels.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('chat.{id}', function ($user, $id) {
+Broadcast::channel('user.status.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
+Broadcast::channel('chat.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
