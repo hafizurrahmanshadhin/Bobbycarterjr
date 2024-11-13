22abdc04b4c53a379a21652c77c63bf32e76e2b2
Rhishi Kesh <reshikash300@gmail.com>
2024-11-13 15:46:02 +0600

    User Update API Modify

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -56,7 +56,7 @@ class UserController extends Controller
 
             // If user is not found, return an error response
             if ($user->id !== auth()->id()) {
-                return Helper::jsonResponse(false, 'This User not authenticated.', 401, []);
+                return Helper::jsonResponse(false, 'Unauthorized Access!', 404);
             }
 
             if ($request->hasFile('avatar')) {
