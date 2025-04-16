6f6a1fc1514e2290e5a5fcbc01895a3bc55d4068
Rhishi-kesh <reshikash300@gmail.com>
2025-04-16 16:26:53 +0600

    Dynamic Page Content

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function termsAndConditions()
+    public function appTerms()
     {
         $dynamicPage = DynamicPage::query()
             ->where('status', 'active')
