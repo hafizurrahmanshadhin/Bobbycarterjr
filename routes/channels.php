facd40c89edcc0f916f1cf13db4bfaf14d969cd3
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-04-28 14:26:34 +0600

    In channel.php added tru directly

--- routes/channels.php
+++ routes/channels.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('user.status.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    // return (int) $user->id === (int) $id;
+    return true;
 });
 
 Broadcast::channel('chat.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    // return (int) $user->id === (int) $id;
+    return true;
 });
